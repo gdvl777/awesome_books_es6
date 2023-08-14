406e6633d7b86ac81a3cfbf24105314e48c0f781
gdvl777 <gdvl777@gmail.com>
2023-08-14 15:25:40 -0500

    w/ES6

---
+++ modules/dateManager.js
@@ -0,0 +1,16 @@
+import { DateTime } from './luxon.js';
+
+const displayDate = (elementId) => {
+  const dateElement = document.getElementById(elementId);
+
+  const updateTime = () => {
+    const today = DateTime.now();
+    const format = today.toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
+    dateElement.textContent = format;
+  };
+
+  updateTime();
+  setInterval(updateTime, 1000);
+};
+
+export default displayDate;
